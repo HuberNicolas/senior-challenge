f1619954ba2ecced12b6a784c5a9e409ce06ac67
Nicolas Huber <nicolas.huber2@uzh.ch>
2023-07-24 21:02:55 +0200

    Implementation of Task 3

--- app/src/pages/jobs/[id].tsx
+++ app/src/pages/jobs/[id].tsx
@@ -23,11 +23,26 @@ function Details() {
 
   else {
     return (
-      <div>
-        <h1>{job.title}</h1>
-        <p>{job.description}</p>
+      <div className="flex flex-col items-center">
+        <h1 className="max-w-sm p-4 m-auto mb-2 text-2xl font-bold">Jobs Details</h1>
+        <div className="flex flex-wrap mb-2">
+          <div className="w-full max-w-sm p-4 m-auto mt-4 border rounded-lg hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" key={"job" + job.id}>
+            <a href={"/jobs/" + job.id}>
+              <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{job.title}</h5>
+            </a>
+            <p className="font-normal text-gray-900 dark:text-gray-600">{job.company}</p>
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">{job.description}</p>
+            <p>{"Employment rate: " + job.percent + "%"}</p>
+          </div>
+        </div>
+        <a href="/">
+          <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+            Back
+          </button>
+        </a>
       </div>
-    )
+    );
+
   }
 }
 
