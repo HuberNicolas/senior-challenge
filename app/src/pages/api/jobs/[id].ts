bc0dd8690d4b5d5dc0f3c85b18797c7332873765
Nicolas Huber <nicolas.huber2@uzh.ch>
2023-07-24 20:34:14 +0200

    Task2: Fixed API Error (dynamic route)

--- app/src/pages/api/jobs/[id].ts
+++ app/src/pages/api/jobs/[id].ts
@@ -1,13 +1,25 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { JOBS } from '../../../data'
-
-// this API endpoint should return the job as requested by ID and should be adapted to work
-// it currently always returns the job with ID 1
-// HINT: https://nextjs.org/docs/api-routes/dynamic-api-routes might be helpful
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Job>
+  res: NextApiResponse<Job | { error: string }>
 ) {
-  res.status(200).json(JOBS[1])
+  // get query parameter
+  const { id } = req.query
+
+  // parse to int
+  const jobId = parseInt(id as string, 10);
+
+  // number of jobs to define upper border
+  const jobNumber = Object.keys(JOBS).length;
+
+  // validation: number && >= 1 && <= #jobs
+  if (isNaN(jobId) || jobId < 1 || jobId >= jobNumber) {
+    // If the provided ID is not valid, return an error response
+    res.status(404).json({ error: 'Job not found' }); // resolving by adding another type for response
+  } else {
+    // Return the job with the specified ID
+    res.status(200).json(JOBS[jobId]);
+  }
 }
