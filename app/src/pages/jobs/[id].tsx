c41cf74dec7be8a6d225bfc7463feacff1529c9d
Nicolas Huber <nicolas.huber2@uzh.ch>
2023-07-24 20:53:55 +0200

    Task 2: Get task dynamically

--- app/src/pages/jobs/[id].tsx
+++ app/src/pages/jobs/[id].tsx
@@ -1,8 +1,34 @@
-// HINT: Story 2: the following might be helpful when working with dynamic routes
-// Next.js Routing: https://nextjs.org/docs/routing/introduction
-
+import useSWR from 'swr'
+import { useRouter } from 'next/router'
 function Details() {
-  return <div />
+  const router = useRouter();
+  const { id } = router.query; // Extract the dynamic 'id' parameter from the router
+
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+  // Use the dynamic 'id' parameter to construct the API URL
+  // access data via SWR from api using defined fetcher
+  const { data: job, error } = useSWR<Job, Error>(`/api/jobs/${id}`, fetcher);
+  console.log(job)
+
+  // catch error
+  if (error) {
+    return <div>Error occurred while fetching data.</div>;
+  }
+
+  // something went wrong
+  if (!job) {
+    return <div>Loading...</div>;
+  }
+
+  else {
+    return (
+      <div>
+        <h1>{job.title}</h1>
+        <p>{job.description}</p>
+      </div>
+    )
+  }
 }
 
 export default Details
