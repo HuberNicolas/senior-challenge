19a099f37e71c0f52f4544430847fb80292ab0d0
Nicolas Huber <nicolas.huber2@uzh.ch>
2023-07-24 20:54:28 +0200

    Format

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -19,7 +19,7 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json())
 function Index() {
 
   // access data via SWR from api using defined fetcher
-  const {data: jobs} = useSWR<Job[]>('/api/jobs', fetcher)
+  const { data: jobs } = useSWR<Job[]>('/api/jobs', fetcher)
   console.log(jobs) // DEBUG
 
   const jobList = jobs ?? [];
@@ -29,15 +29,15 @@ function Index() {
       <h1 className="max-w-sm p-4 m-auto mb-2 text-2xl font-bold">Jobs List</h1>
       <div className="flex flex-wrap">
         {jobList.map((job) => (
-        <div className="w-full max-w-sm p-4 m-auto mt-4 border rounded-lg hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" key={"job"+job.id}>
-          <a href={"/api/jobs/"+job.id}>
-            <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{job.title}</h5>
-          </a>
-          <p className="font-normal text-gray-900 dark:text-gray-600">{job.company}</p>
-          <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">{job.description}</p>
-          <p>{"Employment rate: " + job.percent + "%"} </p>
-        </div>
-      ))}
+          <div className="w-full max-w-sm p-4 m-auto mt-4 border rounded-lg hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" key={"job" + job.id}>
+            <a href={"/api/jobs/" + job.id}>
+              <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{job.title}</h5>
+            </a>
+            <p className="font-normal text-gray-900 dark:text-gray-600">{job.company}</p>
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">{job.description}</p>
+            <p>{"Employment rate: " + job.percent + "%"} </p>
+          </div>
+        ))}
       </div>
     </div>
   )
